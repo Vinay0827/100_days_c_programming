2d2518a8e0d5e2751acd76c93a18649415055298
Vinay jangid <64066407+Vinay0827@users.noreply.github.com>
2023-09-01 22:19:56 +0530

    Update and rename palindrome.c to sum_of_2_num.c

---
+++ day_2/sum_of_2_num.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int a , b ;
+    printf("enter the first no = ");
+    scanf("%d",&a);
+    printf("enter the second no = ");
+    scanf("%d",&b);
+    printf("the sum of %d and %d is = %d", a ,b , a+b);
+    return 0;
+}
