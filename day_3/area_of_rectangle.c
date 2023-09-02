6f069f12b8f9ad89b3171dc7fb430feb3b27f963
Vinay jangid <64066407+Vinay0827@users.noreply.github.com>
2023-09-02 09:15:02 +0530

    Update and rename odd_or_even.c to area_of_rectangle.c

---
+++ day_3/area_of_rectangle.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int l , b ;
+    printf("enter the length of the rectangle = ");
+    scanf("%d",&l);
+    printf("enter the breath of the rectangle = ");
+    scanf("%d",&b);
+    printf("the area of the rectangle is = %d",l*b);
+    
+    return 0;
+}
