f23d7ce8725d4cee8a9ed7cdfe27b2cd201f32d2
Vinay jangid <64066407+Vinay0827@users.noreply.github.com>
2023-09-19 01:57:13 +0530

    Create sq.c

---
+++ day_16/sq.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int side;
+
+    printf("Enter the side length of the square: ");
+    scanf("%d", &side);
+
+    for (int i = 1; i <= side; i++) {
+        for (int j = 1; j <= side; j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
