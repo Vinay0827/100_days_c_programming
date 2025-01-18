9abc2dc372a0d10a9aa0b688c987f2be4736597d
ShreyaO-O <154344168+ShreyaO-O@users.noreply.github.com>
2025-01-18 11:35:12 +0530

    bubble sort

---
+++ day_27/day_27.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+// Function to perform Bubble Sort
+void bubbleSort(int arr[], int n) {
+    int temp;
+    int swapped;
+
+    for (int i = 0; i < n - 1; i++) {
+        swapped = 0; // Keep track of swapping
+
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                swapped = 1;
+            }
+        }
+
+        if (swapped == 0) {
+            break;
+        }
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int n;
+    printf("enter size of array: ");
+    scanf("%d",&n);
+
+    int arr[n];
+    printf("enter elements of array: ");
+    for(int i=0; i<n; i++)
+    {
+    scanf("%d", &arr[i]);
+    }
+    
+    printf("Original array: ");
+    printArray(arr, n);
+
+    bubbleSort(arr, n);
+
+    printf("Sorted array: ");
+    printArray(arr, n);
+
+    return 0;
+}
+
